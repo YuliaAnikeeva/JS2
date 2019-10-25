c40704e0d27563e23fa4e8372f6eebfcb1864247
YuliaAnikeeva <yulia-anikeeva@yandex.ru>
2019-10-25 10:46:12 +0300

    Product list and items from network

--- eShop/script.js
+++ eShop/script.js
@@ -1,120 +1,131 @@
-var userCart = [];
+const API_URL = 'https://raw.githubusercontent.com/YuliaAnikeeva/GitTest/master/catforeshop.json';
 
 document.querySelector('.cart-button').addEventListener('click', () => {
     document.querySelector('.cart-block').classList.toggle('invisible');
   });
-  
-  function makeGETRequest (url) {
-    return fetch (url)
-        .then(result => result.json())
-        
-        .catch (err => {console.log (err)})
-  }
-class GoodsItem {
-  constructor(title, price, image) {
-    this.title = title;
-    this.price = price;
-    this.image = image;
-  }
-  render() {
-    return `<div class="goods-item"><img src="${this.image}" alt="${this.title}"><h3>${this.title}</h3><p>${this.price}</p> <button class="item-button" type="button">Купить</button></div>`;
-  }
-}
-const API_URL = 'https://raw.githubusercontent.com/YuliaAnikeeva/GitTest/master/catforeshop.json';
-
-
-/* function makeGETRequest(url, callback) {
-  let xhr;
-  xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      callback(xhr.responseText);
+ 
+  class List {
+    //суперкласс для ProductsList и Cart
+    constructor (url, container) {
+        this.container = container
+        this.url = url
+        this.items = []
+        this.DTO = ''
+        this.renderedItems = []
+        this._init ()
     }
-  }
+    _init () {
+        return false
+    }
+    async getJSON (url) {
+        try {
+            this.items = await fetch (API_URL)
+                .then (data => data.json ()) 
+                .then (res => {
+                    this.DTO = res
+                    return res.contents ? this.items = res.contents : this.items = res
+                })
+        } 
+        catch (err) {
+            console.log (err)
+        }
+    }
+    render () {
+        const block = document.querySelector (this.container)
+        for (let item of this.items) {
+            let prod = new lists [this.constructor.name] (item)
+            this.renderedItems.push (prod)
+            block.insertAdjacentHTML ('beforeend', prod.render ())
+        }
+    }
+    filter () {
+        //потом
+    }
+}
 
-  xhr.open('GET', url, true);
-  xhr.send();
-} */
+class Item {
+    //суперкласс для ProductsItem и CartItem
+    constructor (el, img) {
+        this.title = el.title
+        this.price = el.price
+        //this.id_product = el.id_product
+        this.image = el.image
+    }
+
+    render() {
+      return `<div class="goods-item">
+                <img src="${this.image}" alt="${this.title}">
+                  <h3>${this.title}</h3>
+                  <p>${this.price}</p> 
+                  <button class="buy-btn" 
+                        data-name="${this.title}"
+                        data-image="${this.image}"
+                        data-price="${this.price}">Купить</button>
+              </div>`;
+    }
+}
+
+class Product  extends Item {}
 
 
-class GoodsList {
-  constructor() {
-    this.goods = [];
+class ProductsList extends List {
+  constructor(cart, url=API_URL , container ='.products') {
+    super (url, container)
+    this.cart = cart
     
   }
 
-
-  /* fetchGoods(cb) {
-    makeGETRequest(API_URL, (goods) => {
-      this.goods = JSON.parse(goods);
-      this.filteredGoods = JSON.parse(goods);
-      cb();
-    })
-  } */
-
-
-  fetchGoods() {
-    makeGETRequest(API_URL)
-        .then (data => {
-            this.goods = data;
-            this.filteredGoods = data;
-            this.render()
-        })      
-}
-render() {
-  let listHtml = '';
-  this.filteredGoods.forEach(good => {
-    const goodItem = new GoodsItem(good.title, good.price, good.image);
-    listHtml += goodItem.render();
-  });
-  document.querySelector('.goods-list').innerHTML = listHtml;
-}
-  filterGoods(value) {
-    // Здесь будем фильтровать список товаров
-    const regexp = new RegExp(value, 'i');
-    this.filteredGoods = this.goods.filter(good => regexp.test(good.title));
-    this.render();
-
+  async _init() {
+    await this.getJSON()
+    this.render()
   }
-
 }
-document.querySelector('.search-button').addEventListener('click', (e) => {
-  const value = document.querySelector('.goods-search').value;
-  list.filterGoods(value);
-})
-const list = new GoodsList();
-list.fetchGoods(() => {
-  list.render();
-});
+  
 
 
 //User cart
 
-class CartItem {
-  constructor(title, price) {
-    this.title = title;
-    this.price = price;
-  }
-  render() {
-    return `<div class="cart-item"><h5>${this.title}</h5><p>${this.price}</p>`;
-  }
-}
+// class CartItem extends Item{
+//     constructor (el, img) {
+//         super (el, img)
+//         this.quantity = el.quantity
 
-/* class Cart {
-  constructor() {
-    this.goods = [];
-  }
-  fetchCart() {
-      
-  };
-    
-    renderCart () { 
-    let cartList = '';
-  this.goods.forEach(good => {
-      const Item = new cartItem(good.title, good.price);
-      cartList += CartItem.render();
-    });
-    document.querySelector('.cart-block').innerHTML = cartList;
-  }
+//         // this.product_name = el.product_name
+//         // this.price = el.price
+//         // //this.id_product = el.id_product
+//         // this.img = el.img
+//     }
+//     render (){
+//         return `<div class="cart-item">
+//         <div class="product-bio">
+//             <img src="${this.img}" alt="Some image">
+//             <div class="product-desc">
+//                 <p class="product-title">${this.product_name}</p>
+//                 <p class="product-quantity">Quantity: ${this.quantity}</p>
+//                 <p class="product-single-price">$${this.price} each</p>
+//             </div>
+//         </div>
+//         <div class="right-block">
+//             <p class="product-price">${this.quantity * this.price}</p>
+//             <button class="del-btn" data-id="${this.id_product}">&times;</button>
+//         </div>
+//     </div>`
+//     }
+//  }
+// class Cart extends List{
+// constructor (url, container = '.cart-block'){
+//     super (url, container)
+//     this.data = {}
+
+// }
+// async _init(){
+//     await this.getJSON()
+// }
+// }
+
+const lists = {
+    ProductsList: Product,
+ //   Cart: CartItem
 }
- */
+let products = new ProductsList ()
+//let cart = new Cart ()
